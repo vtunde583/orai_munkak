0c6cf7a6b27b570bbd3312ae09c7a6bed3f04de5
vtunde583 <vartun583@hengersor.hu>
2025-01-27 12:10:56 +0100

    orai feladat

--- kozut/backend/server.js
+++ kozut/backend/server.js
@@ -1,21 +1,55 @@
 const express = require("express");
-const app= express();
+const app = express();
 const cors = require("cors");
-const mysql= require("mysql");
-
+const mysql = require("mysql");
+ 
+app.use(express.json());
 app.use(cors());
-const db =mysql.createConnection({
-    user:"root",
+ 
+const db = mysql.createConnection({
+    user: "root",
     host: "127.0.0.1",
-    port:3307,
+    port: "3306",
     password: "",
     database: "kozutak",
 });
-
-app.get("/", (req, res)=> {
-    res.send("fut a backend");
-})
-
-app.listen(3001, ()=> {
-    console.log("server is running on port 3001");
+ 
+app.get("/", (req, res) =>{
+    res.send("Fut a backend.");
 });
+ 
+app.get("/regiok", (req, res) => {
+    const sql = "SELECT * FROM regiok";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+ 
+app.get("/regiok_8", (req, res) => {
+    const sql = "SELECT * FROM regiok WHERE Rid = 8";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+ 
+app.post("/ujregio", (req, res) => {
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES (NULL, ?, ?)";
+    const values = [
+        '9', "Dél Alföld', 'régió', '10', 'Szeged', 'Főváros' "
+    ];
+ 
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.error("hiba történt", err);
+            return res.status(500).json({ error: "Adatbázis hiba történt" });
+        }
+        return res.status(200).json({ message: "Sikeres beszúrás!", result });
+    });
+});
+ 
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+
