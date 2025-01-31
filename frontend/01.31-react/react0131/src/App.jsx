46c3e80e04993880ce09ea920ad21e9cfd0573a1
vtunde583 <vartun583@hengersor.hu>
2025-01-31 14:19:12 +0100

    kozut.01.31

---
+++ frontend/01.31-react/react0131/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <> 
+    <h1> kozut</h1>
+    
+   <div>
+
+   </div>
+  
+   </>    
+    
+  )
+}
+
+export default App
